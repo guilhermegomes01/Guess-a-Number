3713c41fc8fe8d96398de82318d9373a71e290d2
Guilherme Gomes <guiga.gomes10@gmail.com>
2020-05-22 10:54:53 -0300

    Custom theme e colors

---
+++ constants/fonts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  body: {
+    fontFamily: "open-sans",
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+    fontFamily: "open-sans-bold",
+  },
+});
